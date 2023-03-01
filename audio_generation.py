01cbb923ebe992e2bcf44bccfeb51cab0367cab9
Rain <a.khraisat@xina.tech>
2023-03-01 18:02:07 +0300

    audio enhancement & generation base classes

---
+++ audio_generation.py
@@ -0,0 +1,55 @@
+import base64
+import io
+import os
+from typing import Tuple
+import yaml
+import requests
+import torch
+import torchaudio
+
+
+class SpeechGenerator:
+    def __init__(self,
+                 tts_url: str = "https://65n1fnjut7.execute-api.eu-west-1.amazonaws.com/dev/v1/tts-api-manager",
+                 token_header_key: str = "x-api-key",
+                 token_variable_name: str = "TTS_TOKEN",
+                 sample_rate: int = 16000,
+                 content_type: str = "application/json"):
+        self.tts_url = tts_url
+
+        access_token = os.environ.get(token_variable_name)
+
+        self.request_headers = {token_header_key: access_token,
+                                "Content-Type": content_type,
+                                "sample-rate": str(sample_rate)}
+
+    def send_request(self, text: str, language_symbol: str) -> requests.Response:
+        contents = {"text": text,
+                    "as_url": False,
+                    "lang": language_symbol}
+        return requests.post(url=self.tts_url,
+                             json=contents,
+                             headers=self.request_headers)
+
+    def _decode_response(self, response: requests.Response) -> bytes:
+        response_dict = response.json()
+        audio = response_dict["audio"]['audio_data']
+        return base64.b64decode(audio)
+
+    def _read_audio_bytes(self, audio_bytes: bytes) -> Tuple[torch.Tensor, int]:
+        buffered_audio = io.BytesIO(audio_bytes)
+        y, sr = torchaudio.load(buffered_audio)
+        y = y.detach().cpu()
+        return y, sr
+
+    def generate_audio(self, text: str, language_symbol: str) -> Tuple[torch.Tensor, int]:
+        response = self.send_request(text, language_symbol)
+        audio_bytes = self._decode_response(response)
+        audio_tensor, sample_rate = self._read_audio_bytes(audio_bytes)
+        return audio_tensor, sample_rate
+
+
+def load_speech_generator(configs_file_path: str = "configs.yaml"):
+    with open(configs_file_path, "r") as f:
+        configs_dict = yaml.safe_load(f)
+    return SpeechGenerator(**configs_dict['speech_enhancement'])
---
+++ audutils.py
@@ -0,0 +1,73 @@
+from typing import Tuple
+
+import torch
+import torchaudio
+import yaml
+from speechbrain.pretrained import SpectralMaskEnhancement
+
+
+class SpeechEnhancer:
+    def __init__(self,
+                 pretrained_model: str = "speechbrain/metricgan-plus-voicebank",
+                 save_pretrained_dir: str = r"./speech-enhancement-model",
+                 device: str = "cuda",
+                 volume_gain: float = 1.1,
+                 fade_in_ratio: float = 0.30,
+                 fade_out_ratio: float = 0.5,
+                 fade_shape: str = 'linear',
+                 gain_type: str = '-n',
+                 pitch: int = 40,
+                 new_sr: int = 48000,
+                 tempo: float = 1.15):
+        self.device = device if torch.cuda.is_available() else 'cpu'
+        self.model = SpectralMaskEnhancement.from_hparams(
+            source=pretrained_model,
+            savedir=save_pretrained_dir,
+            run_opts={"device": self.device})
+
+        self.volume_changer = torchaudio.transforms.Vol(gain=volume_gain, gain_type="amplitude")
+
+        fade_in_steps = int(new_sr * fade_in_ratio)
+        fade_out_steps = int(new_sr * fade_out_ratio)
+        self.fade_in_out_applier = torchaudio.transforms.Fade(fade_in_len=fade_in_steps,
+                                                              fade_out_len=fade_out_steps,
+                                                              fade_shape=fade_shape)
+
+        self.gain_type = gain_type
+        self.pitch_modifier = pitch
+        self.new_sr = new_sr
+        self.tempo = tempo
+
+        self.sox_effects = [
+            ['gain', self.gain_type],
+            ['pitch', str(self.pitch_modifier)],
+            ['rate', str(self.new_sr)],
+            ['tempo', str(self.tempo)],
+        ]
+
+    def _enhance_speech(self, speech_tensor: torch.Tensor) -> torch.Tensor:
+        speech_tensor = speech_tensor.squeeze().to(self.device)
+        return self.model.enhance_batch(speech_tensor, lengths=torch.tensor([1.]))
+
+    def _apply_sox_effects(self, speech_tensor: torch.Tensor, sample_rate: int) -> Tuple:
+        return torchaudio.sox_effects.apply_effects_tensor(speech_tensor,
+                                                           effects=self.sox_effects,
+                                                           sample_rate=sample_rate)
+
+    def _apply_transforms(self, speech_tensor: torch.Tensor) -> torch.Tensor:
+        speech_tensor = speech_tensor.cpu()
+        speech_tensor = self.fade_in_out_applier(speech_tensor)
+        return self.volume_changer(speech_tensor)
+
+    @torch.inference_mode()
+    def __call__(self, speech_tensor: torch.Tensor, sample_rate: int) -> Tuple[torch.Tensor, int]:
+        speech_tensor = self._enhance_speech(speech_tensor)
+        speech_tensor, sample_rate = self._apply_sox_effects(speech_tensor, sample_rate)
+        speech_tensor = self._apply_transforms(speech_tensor)
+        return speech_tensor, sample_rate
+
+
+def load_speech_enhancer(configs_file_path: str = "configs.yaml"):
+    with open(configs_file_path, "r") as f:
+        configs_dict = yaml.safe_load(f)
+    return SpeechEnhancer(**configs_dict['speech_generation'])
